50420f236d2cc51b2fa9ea6cd76330adf212e040
Hiyokokeko <sukepon0914@gmail.com>
2020-04-02 00:36:53 +0900

    RedirectToGroupMessagesPath
    # What
    グループ編集・更新後にルートパスへリダイレクト処理
    # Why
    仮表示であったため

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -8,7 +8,7 @@ class GroupsController < ApplicationController
   def create
     @group = Group.new(group_params)
     if @group.save
-      redirect_to root_path, notice: 'グループを作成しました'
+      redirect_to group_messages_path(@group), notice: 'グループを作成しました'
     else
       render :new
     end
@@ -21,7 +21,7 @@ class GroupsController < ApplicationController
   def update
     @group = Group.find(params[:id])
     if @group.update(group_params)
-      redirect_to root_path, notice: 'グループを更新しました'
+      redirect_to group_messages_path(@group), notice: 'グループを更新しました'
     else
       render :edit
     end
@@ -32,4 +32,7 @@ class GroupsController < ApplicationController
     params.require(:group).permit(:name, user_ids: [])
   end
 
+  def set_group
+    @group = Group.find(params[:group_id])
+  end
 end
