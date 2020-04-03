337a05cc239f6baf9946c700276c9efcf69f79d5
Hiyokokeko <sukepon0914@gmail.com>
2020-04-03 14:34:01 +0900

    FactoryBotの使用準備
    what
    テストで用いるFactoryBotの準備
    why
    FactoryBotの使用により記述を省略することができるため

---
+++ spec/factories/users.rb
@@ -0,0 +1,8 @@
+FactoryBot.define do
+  factory :user do
+    password = Faker::Internet.password(min_length: 8)
+    name {Faker::name.last_name}
+    password {password}
+    password_confirmation {password}
+  end
+end
